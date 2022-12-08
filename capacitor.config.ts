9fb4fb39597f163f0d6269c989c0fe61a6c0bce2
Mirko Nasato <mirko@nasato.dev>
2022-12-08 16:52:35 +0000

    Live Reload on Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'org.example.biorhythms',
   appName: 'Biorhythms',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  // server: {
+  //   url: 'http://192.168.1.120:3000',
+  //   cleartext: true,
+  // },
 };
 
 export default config;
